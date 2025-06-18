534cb3e53409c46a01f161348abd255992d1af05
tolk-vm <tolk.vm@gmail.com>
2025-06-18 12:18:53 +0700

    [06] convert to Tolk v0.13

--- wrappers/06_vesting/VestingWallet.compile.ts
+++ wrappers/06_vesting/VestingWallet.compile.ts
@@ -1,6 +1,10 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['vesting_wallet.fc'],
+    // lang: 'func',
+    // targets: ['vesting_wallet.fc'],
+    lang: 'tolk',
+    entrypoint: 'vesting_wallet.tolk',
+    withSrcLineComments: true,
+    withStackComments: true,
 };
