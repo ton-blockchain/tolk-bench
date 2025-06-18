4f86040f7b59d33c4860a7a8d4d9404536f2443d
tolk-vm <tolk.vm@gmail.com>
2025-06-18 13:07:42 +0700

    [07] convert to Tolk v0.13

--- wrappers/07_telemint/NftCollection.compile.ts
+++ wrappers/07_telemint/NftCollection.compile.ts
@@ -1,6 +1,10 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['nft-collection-no-dns.fc']
+    // lang: 'func',
+    // targets: ['nft-collection-no-dns.fc']
+    lang: 'tolk',
+    entrypoint: 'nft-collection-no-dns.tolk',
+    withSrcLineComments: true,
+    withStackComments: true,
 }
--- wrappers/07_telemint/NftItem.compile.ts
+++ wrappers/07_telemint/NftItem.compile.ts
@@ -1,6 +1,10 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['nft-item-no-dns-cheap.fc']
+    // lang: 'func',
+    // targets: ['nft-item-no-dns-cheap.fc']
+    lang: 'tolk',
+    entrypoint: 'nft-item-no-dns-cheap.tolk',
+    withSrcLineComments: true,
+    withStackComments: true,
 }
