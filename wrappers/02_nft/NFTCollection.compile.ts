6ec9349bd18218d8cba6fdf6244c28711c01b3ed
tolk-vm <tolk.vm@gmail.com>
2025-06-18 10:56:32 +0700

    [02] convert to Tolk v0.13

--- wrappers/02_nft/NFTCollection.compile.ts
+++ wrappers/02_nft/NFTCollection.compile.ts
@@ -1,6 +1,10 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['nft-collection.fc']
+    // lang: 'func',
+    // targets: ['nft-collection.fc']
+    lang: 'tolk',
+    entrypoint: 'nft-collection.tolk',
+    withSrcLineComments: true,
+    withStackComments: true,
 };
--- wrappers/02_nft/NFTItem.compile.ts
+++ wrappers/02_nft/NFTItem.compile.ts
@@ -1,6 +1,10 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['nft-item.fc']
+    // lang: 'func',
+    // targets: ['nft-item.fc']
+    lang: 'tolk',
+    entrypoint: 'nft-item.tolk',
+    withSrcLineComments: true,
+    withStackComments: true,
 };
