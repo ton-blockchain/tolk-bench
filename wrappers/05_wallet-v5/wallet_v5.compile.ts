6becbd8a531596f520ea3709b8698e067317db47
tolk-vm <tolk.vm@gmail.com>
2025-06-18 12:15:35 +0700

    [05] convert to Tolk v0.13

--- wrappers/05_wallet-v5/wallet_v5.compile.ts
+++ wrappers/05_wallet-v5/wallet_v5.compile.ts
@@ -1,6 +1,10 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['wallet_v5.fc']
+    // lang: 'func',
+    // targets: ['wallet_v5.fc']
+    lang: 'tolk',
+    entrypoint: 'wallet_v5.tolk',
+    withSrcLineComments: true,
+    withStackComments: true,
 };
